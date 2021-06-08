83f61e29eab03ac4cd36f7051eea880f0809e31c
Klemen-Z <klemenzmahar@gmail.com>
2021-06-08 11:28:22 +0200

    added if statements

--- Text Adventure/Text_adventure.py
+++ Text Adventure/Text_adventure.py
@@ -221,6 +221,21 @@ def gameuserchoicerules():
                     tom = False
                 else:
                     print('select a choice from the list')
+            elif eventchosen == 5:
+                if gamechoice == 1:
+                    'bob'
+                    tom = False
+                elif gamechoice == 2:
+                    'bob'
+                    tom = False
+                elif gamechoice == 3:
+                    'bob'
+                    tom = False
+                elif gamechoice == 4:
+                    'bob'
+                    tom = False
+                else:
+                    print('select a choice from the list')
 playing = True
 continued = 0
 counter = 0
