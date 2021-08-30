f6eba2e788255dc77531ae80ca7482b10d1876e9
Klemen-Z <klemenzmahar@gmail.com>
2021-08-30 10:15:24 +0200

    Small bug fix delayed

--- Text Adventure/Text_adventure.py
+++ Text Adventure/Text_adventure.py
@@ -112,7 +112,7 @@ def eventrules():
     elif eventchosen == 8:
         event = f'{character} has arrived at a mage academy\n'
     elif eventchosen == 9:
-        event = f'{character} has entered the sleeping forest\n'
+        event = f'{character} has arrived at the sleeping forest\n'
     elif eventchosen == 10:
         event = f'{character} has encountered a mine (of mmmmooooorrrriiaaa)\n'
     log.write(f'''Event number {eventchosen} was chosen, this is the '{event}'\n''')
@@ -247,10 +247,10 @@ def gamechoicerules():
             choicethree = 'Find another path that leads far away'
             choicefour = 'Cast a spell to avoid the curse of the forest'
         elif character == 'Oberon':
-            choiceone = 'Attack'
-            choicetwo = 'Ignore them'
-            choicethree = 'Sneak past them'
-            choicefour = 'No special available'
+            choiceone = 'Enter'
+            choicetwo = 'Go around the forest'
+            choicethree = 'Find another path that leads far away'
+            choicefour = 'Talk to the trees for advice'
         elif character == 'Terra':
             choiceone = 'Enter the forest'
             choicetwo = 'Find another path'
@@ -480,6 +480,7 @@ def gameuserchoicerules():
                         break
                     elif gamechoice == 4:
                         print('Pick something else this is unavailable')
+                        break
                     else:
                         print('Pick an option from the list')
                 tom = False
